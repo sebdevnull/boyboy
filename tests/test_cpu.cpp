5a5a1aa3ccb4d834794c3e186ef3a6bb6d5ade5a
Sebastian A. <sebdevnull@proton.me>
2025-09-08 02:37:45 +0200

    test(cpu): add tests for register and flag manipulation

---
+++ tests/test_cpu.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file test_cpu.cpp
+ * @brief Unit tests for the Cpu class in the BoyBoy emulator.
+ *
+ * @license GPLv3 (see LICENSE file)
+ */
+
+#include <gtest/gtest.h>
+
+#include "cpu.h"
+
+using namespace boyboy::cpu;
+
+// Parameterized tests for CPU register access
+class CpuRegister8Test : public ::testing::TestWithParam<std::pair<Register8Name, uint8_t>> {};
+
+TEST_P(CpuRegister8Test, SetAndGetRegister8)
+{
+    Cpu cpu;
+    auto [reg, value] = GetParam();
+
+    cpu.set_register(reg, value);
+    EXPECT_EQ(cpu.get_register(reg), value);
+}
+
+INSTANTIATE_TEST_SUITE_P(Register8Tests,
+                         CpuRegister8Test,
+                         ::testing::Values(std::make_pair(Register8Name::A, 0x12),
+                                           std::make_pair(Register8Name::B, 0x34),
+                                           std::make_pair(Register8Name::C, 0x56),
+                                           std::make_pair(Register8Name::D, 0x78),
+                                           std::make_pair(Register8Name::E, 0x9A),
+                                           std::make_pair(Register8Name::H, 0xBC),
+                                           std::make_pair(Register8Name::L, 0xDE),
+                                           std::make_pair(Register8Name::F, 0xF0)));
+
+class CpuRegister16Test : public ::testing::TestWithParam<std::pair<Register16Name, uint16_t>> {};
+
+TEST_P(CpuRegister16Test, SetAndGetRegister16)
+{
+    Cpu cpu;
+    auto [reg, value] = GetParam();
+
+    cpu.set_register(reg, value);
+    EXPECT_EQ(cpu.get_register(reg), value);
+}
+
+INSTANTIATE_TEST_SUITE_P(Register16Tests,
+                         CpuRegister16Test,
+                         ::testing::Values(std::make_pair(Register16Name::BC, 0x1234),
+                                           std::make_pair(Register16Name::DE, 0x5678),
+                                           std::make_pair(Register16Name::HL, 0x9ABC),
+                                           std::make_pair(Register16Name::AF, 0xDEF0),
+                                           std::make_pair(Register16Name::SP, 0xFFF0),
+                                           std::make_pair(Register16Name::PC, 0xABCD)));
+
+// Flags tests
+class CpuFlagTest : public ::testing::TestWithParam<std::pair<uint8_t, bool>> {};
+
+TEST_P(CpuFlagTest, SetAndGetFlag)
+{
+    Cpu cpu;
+    auto [flag, value] = GetParam();
+
+    cpu.set_flag(flag, value);
+    EXPECT_EQ(cpu.get_flag(flag), value);
+}
+
+INSTANTIATE_TEST_SUITE_P(FlagTests,
+                         CpuFlagTest,
+                         ::testing::Values(std::make_pair(Flag::Zero, true),
+                                           std::make_pair(Flag::Zero, false),
+                                           std::make_pair(Flag::Carry, true),
+                                           std::make_pair(Flag::Carry, false),
+                                           std::make_pair(Flag::Substract, true),
+                                           std::make_pair(Flag::Substract, false),
+                                           std::make_pair(Flag::HalfCarry, true),
+                                           std::make_pair(Flag::HalfCarry, false)));
