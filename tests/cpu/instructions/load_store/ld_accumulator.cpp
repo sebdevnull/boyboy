bfcb0f7043a984a3f3ac7ccdd19c362a4d7fb863
Sebastian A. <sebdevnull@proton.me>
2025-09-12 00:39:58 +0200

    test(cpu): add tests for load accumulator instructions LD A, [n16] and LD [n16], A

--- tests/cpu/instructions/load_store/ld_accumulator.cpp
+++ tests/cpu/instructions/load_store/ld_accumulator.cpp
@@ -3,11 +3,12 @@
  * @brief Unit tests for load to/from accumulator instructions.
  *
  * LD A, [BC/DE/HL+/-] and LD [BC/DE/HL+/-], A
- * 
+ * LD A, [n16] and LD [n16], A
+ *
  * @license GPLv3 (see LICENSE file)
  */
 
- #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
 // Helpers
 #include "helpers/cpu_fixtures.h"
@@ -22,14 +23,18 @@ using boyboy::test::cpu::R8Test;
 // -----------------------------
 // Test types
 // -----------------------------
-using LdToAccTest = R8Test<R8Param>;
+using LdToAccTest   = R8Test<R8Param>;
 using LdFromAccTest = R8Test<R8Param>;
+using LdAMemTest    = R8Test<R8Param>;
+using LdMemATest    = R8Test<R8Param>;
 
 // -----------------------------
 // Test definitions
 // -----------------------------
 TEST_P(LdToAccTest, Works) { run_test(); }
 TEST_P(LdFromAccTest, Works) { run_test(); }
+TEST_P(LdAMemTest, Works) { run_test(); }
+TEST_P(LdMemATest, Works) { run_test(); }
 
 // -----------------------------
 // Parameter instantiations
@@ -84,4 +89,32 @@ INSTANTIATE_TEST_SUITE_P(LdInstructions,
                                  .expected_value = 0x55,
                                  .name           = "LD_AT_DE_A",
                              }),
-                         boyboy::test::cpu::param_name<R8Param>);                                                  
+                         boyboy::test::cpu::param_name<R8Param>);
+
+// LD a, [n16]
+INSTANTIATE_TEST_SUITE_P(LdInstructions,
+                         LdAMemTest,
+                         ::testing::Values(R8Param{
+                             .opcode         = Opcode::LD_A_AT_A16,
+                             .src_op_type    = OperandType::Memory,
+                             .dst            = Reg8Name::A,
+                             .src_addr       = 0xABC0,
+                             .src_value      = 0xAA,
+                             .expected_value = 0xAA,
+                             .name           = "LD_A_AT_A16",
+                         }),
+                         boyboy::test::cpu::param_name<R8Param>);
+
+// LD [n16], A
+INSTANTIATE_TEST_SUITE_P(LdInstructions,
+                         LdMemATest,
+                         ::testing::Values(R8Param{
+                             .opcode         = Opcode::LD_AT_A16_A,
+                             .dst_op_type    = OperandType::Memory,
+                             .src            = Reg8Name::A,
+                             .dst_addr       = 0x0ABC,
+                             .src_value      = 0x55,
+                             .expected_value = 0x55,
+                             .name           = "LD_AT_A16_A",
+                         }),
+                         boyboy::test::cpu::param_name<R8Param>);
