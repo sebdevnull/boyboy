d488f7ec3456c859a1af2d1c943deb5bfadbd3a5
Sebastian A. <sebdevnull@proton.me>
2025-09-12 00:00:47 +0200

    test(cpu): add tests for load accumulator instructions LD A, [BC/DE] and LD [BC/DE], A

---
+++ tests/cpu/instructions/load_store/ld_accumulator.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file ld_accumulator.cpp
+ * @brief Unit tests for load to/from accumulator instructions.
+ *
+ * LD A, [BC/DE/HL+/-] and LD [BC/DE/HL+/-], A
+ * 
+ * @license GPLv3 (see LICENSE file)
+ */
+
+ #include <gtest/gtest.h>
+
+// Helpers
+#include "helpers/cpu_fixtures.h"
+#include "helpers/cpu_params.h"
+
+using namespace boyboy::cpu;
+
+using boyboy::test::cpu::OperandType;
+using boyboy::test::cpu::R8Param;
+using boyboy::test::cpu::R8Test;
+
+// -----------------------------
+// Test types
+// -----------------------------
+using LdToAccTest = R8Test<R8Param>;
+using LdFromAccTest = R8Test<R8Param>;
+
+// -----------------------------
+// Test definitions
+// -----------------------------
+TEST_P(LdToAccTest, Works) { run_test(); }
+TEST_P(LdFromAccTest, Works) { run_test(); }
+
+// -----------------------------
+// Parameter instantiations
+// -----------------------------
+// LD A, [BC/DE]
+INSTANTIATE_TEST_SUITE_P(LdInstructions,
+                         LdToAccTest,
+                         ::testing::Values(
+                             R8Param{
+                                 .opcode         = Opcode::LD_A_AT_BC,
+                                 .src_op_type    = OperandType::Indirect,
+                                 .src            = Reg16Name::BC,
+                                 .dst            = Reg8Name::A,
+                                 .src_addr       = 0xABC0,
+                                 .src_value      = 0xAA,
+                                 .expected_value = 0xAA,
+                                 .name           = "LD_A_AT_BC",
+                             },
+                             R8Param{
+                                 .opcode         = Opcode::LD_A_AT_DE,
+                                 .src_op_type    = OperandType::Indirect,
+                                 .src            = Reg16Name::DE,
+                                 .dst            = Reg8Name::A,
+                                 .src_addr       = 0xADE0,
+                                 .src_value      = 0x55,
+                                 .expected_value = 0x55,
+                                 .name           = "LD_A_AT_DE",
+                             }),
+                         boyboy::test::cpu::param_name<R8Param>);
+
+// LD [BC/DE], A
+INSTANTIATE_TEST_SUITE_P(LdInstructions,
+                         LdFromAccTest,
+                         ::testing::Values(
+                             R8Param{
+                                 .opcode         = Opcode::LD_AT_BC_A,
+                                 .dst_op_type    = OperandType::Indirect,
+                                 .src            = Reg8Name::A,
+                                 .dst            = Reg16Name::BC,
+                                 .dst_addr       = 0x0BCA,
+                                 .src_value      = 0xAA,
+                                 .expected_value = 0xAA,
+                                 .name           = "LD_AT_BC_A",
+                             },
+                             R8Param{
+                                 .opcode         = Opcode::LD_AT_DE_A,
+                                 .dst_op_type    = OperandType::Indirect,
+                                 .src            = Reg8Name::A,
+                                 .dst            = Reg16Name::DE,
+                                 .dst_addr       = 0x0DEA,
+                                 .src_value      = 0x55,
+                                 .expected_value = 0x55,
+                                 .name           = "LD_AT_DE_A",
+                             }),
+                         boyboy::test::cpu::param_name<R8Param>);                                                  
