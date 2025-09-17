f5d07ce02f48e1635aed3ddd1d002effbd29ae6b
Sebastian A. <sebdevnull@proton.me>
2025-09-17 19:35:41 +0200

    feat(timer): implement I/O Timer component

---
+++ include/boyboy/io/timer.h
@@ -0,0 +1,65 @@
+/**
+ * @file timer.h
+ * @brief Timer operations for BoyBoy emulator.
+ *
+ * @license GPLv3 (see LICENSE file)
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+#include "boyboy/cpu/interrupts.h"
+
+namespace boyboy::io {
+
+class Timer {
+public:
+    void tick(uint16_t cycles);
+
+    [[nodiscard]] uint8_t read(uint16_t addr) const;
+    void write(uint16_t addr, uint8_t value);
+
+    void start();
+    void stop(); // Used by STOP instruction
+    [[nodiscard]] bool is_stopped() const { return stopped_; }
+
+    void set_request_interrupt_callback(cpu::InterruptRequestCallback callback)
+    {
+        request_interrupt_ = std::move(callback);
+    }
+
+private:
+    struct Flags {
+        static constexpr uint8_t TimerEnable = 0x04;  // Bit 2 - Timer Enable
+        static constexpr uint8_t ClockSelect0 = 0x01; // Bit 0 - Input Clock Select bit 0
+        static constexpr uint8_t ClockSelect1 = 0x02; // Bit 1 - Input Clock Select bit 1
+        static constexpr uint8_t ClockSelectMask = ClockSelect0 | ClockSelect1;
+
+        static constexpr uint8_t Clock256M = 0x00; // 4096 Hz
+        static constexpr uint8_t Clock4M = 0x01;   // 262144 Hz
+        static constexpr uint8_t Clock16M = 0x02;  // 65536 Hz
+        static constexpr uint8_t Clock64M = 0x03;  // 16384 Hz
+
+        // Frequencies in number of cycles per increment
+        static constexpr std::array<uint16_t, 4> ClockFrequencies = {1024, 16, 64, 256};
+    };
+
+    cpu::InterruptRequestCallback request_interrupt_;
+
+    // Timer registers
+    uint8_t div_{0};  // Divider Register
+    uint8_t tima_{0}; // Timer Counter
+    uint8_t tma_{0};  // Timer Modulo
+    uint8_t tac_{0};  // Timer Control
+
+    // Internal counters
+    uint16_t div_counter_{0};
+    uint16_t tima_counter_{0};
+
+    // Timer stopped flag
+    bool stopped_{false};
+};
+
+} // namespace boyboy::io
