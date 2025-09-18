82662cf21b94716cfea14368d6678f8b3602635b
Sebastian A. <sebdevnull@proton.me>
2025-09-18 23:51:46 +0200

    refactor(display): migrate to SDL2 and glad

--- include/boyboy/display.h
+++ include/boyboy/display.h
@@ -7,16 +7,39 @@
 
 #pragma once
 
+#include <string>
+
+#include "boyboy/ppu/ppu.h"
+
+// Forward declarations for SDL and OpenGL types
+class SDL_Window;
+using SDL_GLContext = void*;
+using GLuint = unsigned int;
+
 namespace boyboy::display {
+
+static constexpr int DefaultScale = 4;
 
 class Display {
 public:
-    static void init();
-    void update() {};
+    Display(int scale = DefaultScale) : scale_(scale) {}
+
+    bool init(const std::string& title = "BoyBoy");
+    void shutdown();
+    void poll_events(bool& running);
+    void render_frame(const ppu::FrameBuffer& framebuffer);
 
 private:
-    static constexpr int Width = 160;
-    static constexpr int Height = 144;
+    int width_ = ppu::Width;
+    int height_ = ppu::Height;
+    int scale_;
+
+    SDL_Window* window_ = nullptr;
+    SDL_GLContext gl_context_ = nullptr;
+    GLuint texture_{}, VAO_{}, VBO_{}, EBO_{};
+    GLuint shader_program_{};
+
+    void init_opengl();
 };
 
 } // namespace boyboy::display
