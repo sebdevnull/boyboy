33eeb37c51424a37d1dea59cc321bdcc4d42733f
Sebastian A. <sebdevnull@proton.me>
2025-09-22 23:57:55 +0200

    refactor(logging): change DMA transfer logs from debug to trace level

--- src/boyboy/mmu.cpp
+++ src/boyboy/mmu.cpp
@@ -293,7 +293,7 @@ void Mmu::Dma::start(uint8_t value)
     tick_counter = 0;
     cks = 0;
 
-    log::debug("Starting DMA transfer from {}", utils::PrettyHex(src).to_string());
+    log::trace("Starting DMA transfer from {}", utils::PrettyHex(src).to_string());
 }
 
 void Mmu::Dma::tick(uint16_t cycles, Mmu& mmu)
@@ -320,7 +320,7 @@ void Mmu::Dma::tick(uint16_t cycles, Mmu& mmu)
 
     if (bytes_remaining == 0) {
         active = false;
-        log::debug("DMA transfer completed, checksum: {}", utils::PrettyHex(cks).to_string());
+        log::trace("DMA transfer completed, checksum: {}", utils::PrettyHex(cks).to_string());
         if (cks == 0) {
             log::warn("DMA transfer checksum is zero, possible issue with source data");
         }
