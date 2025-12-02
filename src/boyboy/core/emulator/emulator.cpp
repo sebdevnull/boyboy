bd1f86ba537dc877e62627f8d7a2d891569e183e
Sebastian A. <sebdevnull@proton.me>
2025-12-02 11:04:33 +0100

    refactor(emulator): remove ppu LCD power on since it's not needed anymore

--- src/boyboy/core/emulator/emulator.cpp
+++ src/boyboy/core/emulator/emulator.cpp
@@ -103,10 +103,6 @@ void Emulator::start()
     cartridge_->load_ram();
     display_->init();
 
-    // Enable LCD: it seems that some games expect it to be on at start, probably because the boot
-    // ROM does it so we will enable it until we implement the boot ROM (if we ever do)
-    ppu_->enable_lcd(true);
-
     started_ = true;
 }
 
